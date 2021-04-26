edf8b8b3246887b9fd548f47e874955872decc68
Gilibeth Vega <gvega@creditu.com>
2021-04-26 19:45:04 -0400

    se genera modelo, se instalan gemas

---
+++ db/migrate/20210426231222_create_sales.rb
@@ -0,0 +1,12 @@
+class CreateSales < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sales do |t|
+      t.integer :amount
+      t.string :origin
+      t.string :blend_name
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
